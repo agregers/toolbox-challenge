c0c6c0c0f38999a14c102b4ce74d43d406dec87f
Alexis Gregerson <agregers@uw.edu>
2014-11-25 12:23:42 -0800

    Finished challenge

--- js/app.js
+++ js/app.js
@@ -1,122 +1,171 @@
 // app.js: our main javascript file for this app
 "use strict";
 
-var prevImg;
 var currentImg;
 var currentTile;
 var prevTile;
-
-var tiles = [];
+var prevImg;
+var matches;
+var missed;
+var remaining;;
 var idx;
-
-for (idx = 1; idx <= 32; ++idx) {
-    tiles.push({
-        tileNum: idx,
-        src: 'img/tile' + idx + '.jpg',
-        flipped: false,
-        matched: false
-    });
-} //for each tile
-
-//console.log(tiles);
+var tiles;
+var timer;
 
 //when document is ready..
 $(document).ready(function() {
     $('#start-game').click(function(){
-       console.log('start-game button clicked');
-        tiles = _.shuffle(tiles);
-        var selectedTiles = tiles.slice(0,8);
-        var tilePairs = [];
-        _.forEach(selectedTiles, function(tile) {
-            tilePairs.push(tile);
-            tilePairs.push(_.clone(tile));
-
+        tiles = [];
+        for (idx = 1; idx <= 32; ++idx) {
+            tiles.push({
+            tileNum: idx,
+            src: 'img/tile' + idx + '.jpg',
+            flipped: false,
+            matched: false
         });
-        tilePairs = _.shuffle(tilePairs);
-        console.log(tilePairs);
+    } 
 
-        var gameBoard = $('#game-board');
-        var row = $(document.createElement('div'));
-        var img;
-        _.forEach(tilePairs, function(tile, elemIndex){
-            if(elemIndex > 0 && 0 == elemIndex % 4) {
-                gameBoard.append(row);
-                row = $(document.createElement('div'));
-            }
-           img = $(document.createElement('img'));
-            img.attr({
-               src: 'img/tile-back.png',
-               alt: 'tile ' + tile.tileNum
-            });
-            img.data('tile', tile);
-            row.append(img);
+    window.clearInterval(timer);
+    var gameBoard = $('#game-board');
+    gameBoard.empty();
+    
+    // first tile shuffle
+    tiles = _.shuffle(tiles);
+
+    // first 8 tiles selected
+    var selectedTiles = tiles.slice(0,8);
+    var tilePairs = [];
+
+    // selected 8 are on board
+    _.forEach(selectedTiles, function(tile) {
+        tilePairs.push(_.clone(tile));
+        tilePairs.push(_.clone(tile));
+
+}); 
+    tilePairs = _.shuffle(tilePairs);
+
+    var row = $(document.createElement('div'));
+    var img;
+
+    // tiles are organized in rows of 4
+    _.forEach(tilePairs, function(tile, elemIndex){
+        if(elemIndex > 0 && 0 == (elemIndex % 4)) {
+            gameBoard.append(row);
+            row = $(document.createElement('div'));
+        }
+
+       img = $(document.createElement('img'));
+        img.attr({
+           src: 'img/tile-back.png',
+           alt: 'tile ' + tile.tileNum
         });
-        gameBoard.append(row);
 
-        //get starting milliseconds
-        var startTime = Date.now();
-        window.setInterval(function() {
-            var elapsedSeconds = (Date.now() - startTime) / 1000;
-            elapsedSeconds = Math.floor(elapsedSeconds);
-            if(elapsedSeconds == 1){
-                $('#elapsed-seconds').text(elapsedSeconds + ' second');
-            }
-            else {
-                $('#elapsed-seconds').text(elapsedSeconds + ' seconds');
-            }
-        }, 1000);
+        img.data('tile', tile);
+        row.append(img);
+    }); // end of forEach
+    gameBoard.append(row);
 
-        $('#game-board img').click(function(){
-            currentImg = $(this);
-            currentTile = currentImg.data('tile');
-            flipTile(currentTile, currentImg);
+
+    //get starting milliseconds
+    var startTime = Date.now();
+    timer = window.setInterval(function() {
+        var elapsedSeconds = (Date.now() - startTime) / 1000;
+        elapsedSeconds = Math.floor(elapsedSeconds);
+        if(elapsedSeconds == 1){
+            $('#elapsed-seconds').text(elapsedSeconds + ' second');
+        }
+        else {
+            $('#elapsed-seconds').text(elapsedSeconds + ' seconds');
+        }
+    }, 1000);
+    matches = 0;
+    missed = 0;
+    remaining = 8;
+    update();
+
+    $('#game-board img').click(function(){
+        currentImg = $(this);
+        currentTile = currentImg.data('tile');
+
+        if(currentTile.flipped) {
+            console.log('same tile clicked');
+            return;
+        }
+        flipTile(currentTile, currentImg);
+        if(!prevTile){
+            console.log('first click');
+
+            prevTile = currentTile;
+            prevImg = currentImg;
             console.log(currentTile);
-
-            if(!prevTile){
-                console.log('first click');
-                prevTile = currentTile;
-                prevImg = currentImg;
-            }
-            else if(currentTile === prevTile) {
-                //checkMatch();
-                console.log('its a match!');
-                return;
-            }
-            else {
-                console.log(prevImg);
-                checkMatch();
-                console.log('this is second click');
-            }
-
-        });
-    }); //start game button click
+        }
+        else{
+            console.log('this is second click');
+            checkMatch(currentTile, currentImg);
+        }
+        update();
+    });
+}); //start game button click
 }); //document ready function
 
-function checkMatch(){
+function flipTile(tile, img) {
+    img.fadeOut(100, function() {
+    if(tile.flipped) {
+        if(!tile.matched) {
+            img.attr('src', 'img/tile-back.png');
+        }
+    }
+    else{
+        img.attr('src', tile.src);
+    }
+    tile.flipped = !tile.flipped;
+    img.fadeIn(100);
+}); //end of flipTile
+}
 
-    if(currentTile != prevTile){
+function checkMatch(tile, img){
+    if(currentTile.tileNum == prevTile.tileNum){
+        matches++;
+        remaining--;
+        update();
+        
+        currentTile.matched = true;
+        prevTile.matched = true;
+       
+        if(remaining == 0){
+            console.log('I am finished!');
+            window.setTimeout(function(){
+                window.clearInterval(timer);
+                alert("Congrats! You Won!");
+            }, 250);
+        }
+        currentTile = null;
+        currentImg = null;
+        prevTile = null;
+        prevImg = null;   
+    }
+
+    else{
         console.log('not the same');
+        missed++;
 
         setTimeout(function(){
             flipTile(currentTile, currentImg);
             flipTile(prevTile, prevImg);
-            prevImg = null;
+            currentTile = null;
+            currentImg = null;
             prevTile = null;
+            prevImg = null;
         }, 1000);
-
+        
     }
+
+    update();
 }
 
-function flipTile(tile, img) {
-    img.fadeOut(100, function() {
-        if(tile.flipped) {
-            img.attr('src', 'img/tile-back.png');
-        }
-        else{
-            img.attr('src', tile.src);
-        }
-        tile.flipped = !tile.flipped;
-        img.fadeIn(100);
-    });
-
+function update(){
+    $('#matches-made').text(matches);
+    $('#overall-missed').text(missed);
+    $('#overall-remaining').text(remaining);
 }
+
